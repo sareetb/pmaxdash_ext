87e151fae19cffa3679db2568f9f198e0de00bbf
Sareet <sareet@google.com>
2022-12-28 09:56:49 +0200

    removed date from tcpa_search. unnecessary

--- google_ads_queries/tcpa_search.sql
+++ google_ads_queries/tcpa_search.sql
@@ -1,10 +1,9 @@
 SELECT
-	segments.date AS date,
   customer.id as account_id,
-	campaign.id as campaign_id,
-	segments.conversion_action~0 AS conversion_action_id,
+  campaign.id as campaign_id,
+  segments.conversion_action~0 AS conversion_action_id,
   segments.conversion_action_name AS conversion_name,
- 	bidding_strategy.maximize_conversion_value.target_roas AS bidding_strategy_mcv_troas,
+  bidding_strategy.maximize_conversion_value.target_roas AS bidding_strategy_mcv_troas,
   bidding_strategy.target_roas.target_roas AS bidding_strategy_troas,
   bidding_strategy.maximize_conversions.target_cpa_micros AS bidding_strategy_mc_tcpa,
   bidding_strategy.target_cpa.target_cpa_micros AS bidding_strategy_tcpa,
@@ -16,5 +15,3 @@ FROM
 	campaign
 WHERE
 	campaign.advertising_channel_type = 'SEARCH'
-  AND segments.date >= "{start_date}"
-  AND segments.date <= "{end_date}"
