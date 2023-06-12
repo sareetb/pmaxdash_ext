21fabb1e6d6377e4b722c4c7340e1a89a4c55ad2
sareetb <sareetb@gmail.com>
2023-06-12 09:56:14 +0300

    Update setup.sh to clone google/pmax and deploy dash

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 echo "Cloning repository from github..."
+git clone https://github.com/google/pmax_best_practices_dashboard.git
+
+cd pmax_best_practices_dashboard
+npm init gaarf-wf@latest -- --answers=answers.json
